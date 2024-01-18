0531655564b1112bdf89f2bca420aa62625a689a
Marcio Ferreira Lima <contactmarcioflima@gmail.com>
2024-01-18 15:40:37 -0300

    .

--- exercicio02/js/app.js
+++ exercicio02/js/app.js
@@ -1,4 +1,8 @@
 var a = 5;
 var b = a + 6;
-var total = b
+var total = b;
 document.getElementById("texto").innerHTML = total;
+function chamarFuncao() {
+    alert('Hello, Ze cu');
+    document.body.style.background = 'blue';
+}
