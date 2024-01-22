93c981a6ea80f74971aef1f2a3cf3876c4affe40
Marcio Ferreira Lima <contactmarcioflima@gmail.com>
2024-01-22 15:20:04 -0300

    .

--- exercicio02/js/app.js
+++ exercicio02/js/app.js
@@ -5,11 +5,20 @@ function evento2() {
     document.body.style.background = 'green';
 }
 function evento3() {
-    document.body.style.background = 'red';
+    let button = document.getElementById("Teste")
+    button.style.background = 'red';
 }
 function evento4() {
     document.body.style.background = 'blue';
 }
 function evento5() {
     document.body.style.background = '#ccc';
+}
+function add() {
+    let texto = document.getElementById("Teste");
+    texto.append('cu <br>');
+}
+function normal() {
+    let button = document.getElementById("Teste")
+    button.style.background = '#ccc';
 }
